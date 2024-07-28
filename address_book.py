7ae7a6458bc503b5daca6079352ed650822e047c
NutSheLL <holnaz1337@gmail.com>
2024-07-28 11:12:09 +0300

    Minor fixes v2.1

--- address_book.py
+++ address_book.py
@@ -88,11 +88,11 @@ for name, record in book.data.items():
 john = book.find("John")
 john.edit_phone("1234567890", "1112223333")
 
-print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+print(john) 
 
 # Пошук конкретного телефону у записі John
 found_phone = john.find_phone("5555555555")
-print(f"{john.name.value}: {found_phone}")  # Виведення: 5555555555
+print(f"{john.name.value}: {found_phone}") 
 
 # Видалення запису Jane
 book.delete("Jane")
